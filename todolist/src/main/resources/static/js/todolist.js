f52067c460ba7ef88fcbe0fac6d19f4f99dde603
bugger0330 <bugger0330@naver.com>
2022-05-27 11:18:36 +0900

    프로젝트 연습//완성 하지만 중복되는 코드가 많다ㅠ

--- todolist/src/main/resources/static/js/todolist.js
+++ todolist/src/main/resources/static/js/todolist.js
@@ -4,10 +4,9 @@ let content = document.querySelector(".content");
 
 const deleteBtn = document.querySelectorAll(".delete-btn");
 
-
 load();
 
-
+//조회
 function load(){
 	let url = "/api/v1/todo/list";
 	fetch(url)
@@ -20,13 +19,16 @@ function load(){
 	})
 	.then(result =>{
 		pageLoding(result);
+		
 		modify();
 		remove();
+		
+		
 	})
 	.catch(error => {alert("에러 : "+error);});
 	
 }	
-
+//조회 함수
 function pageLoding(result){
 	
 	let tbodyTr = tbody.querySelector("tr");
@@ -34,9 +36,11 @@ function pageLoding(result){
 	for(let i = 0; i < result.length; i++){
 		tbodyTr += `<tr>
 	                <td>
-	                    <input type="text" class="content" value="${result[i].content}">
+	                    <input type="text" class="content" value="${result[i].content}" id="${result[i].id}">
 	                    <button class="modify-btn">수정</button>
 	                    <button class="delete-btn">삭제</button>
+	                    <button class="yes-btn">예</button>
+	                    <button class="no-btn">아니오</button>
 	                </td>
 	            </tr>
 	            `;
@@ -44,7 +48,7 @@ function pageLoding(result){
 	}
 	
 }
-
+//등록버튼 클릭
 addBtn.onclick = () => {
 	let url = "/api/v1/todo";
 	let option = {
@@ -58,58 +62,83 @@ addBtn.onclick = () => {
 	.then(response => {
 		if(response.ok){
 			alert("등록 완료");
+			load();
 		}else{
 			throw new Error("비동기 처리 오류");
 		}
 	})
 	.catch(error);
 }
+
  
+//수정
 function modify(){
 	let modifyBtn = document.querySelectorAll(".modify-btn");
-	let id = 11;
-	let ddd = document.querySelectorAll("input");
+	let deleteBtn = document.querySelectorAll(".delete-btn");
+	let yesBtn = document.querySelectorAll(".yes-btn");
+	let noBtn = document.querySelectorAll(".no-btn");
+	let inputValue = document.querySelectorAll("input");
+	
+
 	
 	for(let i = 0; i < modifyBtn.length; i++){
 		modifyBtn[i].onclick = () => {
-			console.log("content : "+ddd[i+1].value);
-			if(!confirm("수정하시겠습니까?")){
-				alert("취소 클릭");
-			}else{
-				
+			let id = inputValue[(i+1)].id;
+			console.log("content : "+inputValue[i+1].value);
+			console.log("id : "+inputValue[i+1].id);
+			modifyBtn[i].className = 'change1';
+			deleteBtn[i].className = 'change2';
+			yesBtn[i].className = 'change3';
+			noBtn[i].className = 'change4';
+			
+			yesBtn[i].onclick = () => {
 				url = `/api/v1/todo/${id}`;
 				option = {
 					method : "PUT",
 					headers: {
 			                "Content-Type": "application/json"
 			            },
-					body : JSON.stringify({content : ddd[i+1].value})
+					body : JSON.stringify({
+						content : inputValue[i+1].value,
+						id : id
+						})
 				};
 				fetch(url, option)
 				.then(response => {
 					if(response.ok){
 						alert("수정완료");
+						load();
 					}else{
 						throw new Error("비동기 처리 오류");
 					}
 				})
 				.catch(error);
+			
 			}
+				
 		}
 	}
 }
-	
 
 
+//삭제
 function remove(){
+	let modifyBtn = document.querySelectorAll(".modify-btn");
 	let deleteBtn = document.querySelectorAll(".delete-btn");
-	let id = 11;
+	let yesBtn = document.querySelectorAll(".yes-btn");
+	let noBtn = document.querySelectorAll(".no-btn");
+	let inputValue = document.querySelectorAll("input");
+	
 	
 	for(let i = 0; i < deleteBtn.length; i++){
 		deleteBtn[i].onclick = () => {
-			if(!confirm("삭제하시겠습니까?")){
-				alert("취소 클릭");
-			}else{
+			let id = inputValue[(i+1)].id;
+			modifyBtn[i].className = 'change1';
+			deleteBtn[i].className = 'change2';
+			yesBtn[i].className = 'change3';
+			noBtn[i].className = 'change4';
+			
+			yesBtn[i].onclick = () => {
 				url = `/api/v1/todo/${id}`;
 				option = {
 					method : "DELETE",
@@ -118,6 +147,7 @@ function remove(){
 				.then(response => {
 					if(response.ok){
 						alert("삭제완료");
+						load();
 					}else{
 						throw new Error("비동기 처리 오류");
 					}
@@ -127,7 +157,6 @@ function remove(){
 		}
 	}
 }
-
 
 
 
